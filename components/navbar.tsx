931a0d48200d4c1443661a8ebd9a0edb6447dc1d
Rakesh Valasala <105775467+VALASALARAKESH@users.noreply.github.com>
2024-08-18 21:48:16 +0530

    Designed Sidebar'

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import {cn} from "@/lib/utils"
 import {UserButton} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
 import {ModeToggle} from "@/components/theme-toggle";
+import {MobileSidebar} from "@/components/mobilesidebar"
 
 const font =Poppins({
     weight:"600",
@@ -14,9 +15,9 @@ const font =Poppins({
 export const Navbar=()=>{
     return(
         <div
-            className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            className="fixed h-16 w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <Menu className="block md:hidden"></Menu>
+                <MobileSidebar></MobileSidebar>
                 <Link href='/'>
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
                         FamCampaion.ai
